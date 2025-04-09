3acf45d807668f60935f55ca14d24c23fe9a1dda
Chichi Zhang <chichizhang20000510@gmail.com>
2025-04-08 19:41:17 -0700

    Added UserRepository interface
    Define the UserRepository interface for retrieving a list of users.

--- src/main/java/student/model/User/UserRepository.java
+++ src/main/java/student/model/User/UserRepository.java
@@ -1,4 +1,16 @@
 package student.model.User;
 
+import java.util.List;
+
+/**
+ * Defines methods for accessing user data.
+ */
 public interface UserRepository {
+
+    /**
+     * Retrieves a list of users.
+     *
+     * @return a list of users
+     */
+    List<User> getUsers();
 }
