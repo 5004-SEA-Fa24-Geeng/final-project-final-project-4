993e6cd74a6bb5c24783efc35f1fcb4535a9a860
Chichi Zhang <chichizhang20000510@gmail.com>
2025-04-08 19:42:21 -0700

    Added UserArrayRepository implementation
    Implement UserRepository using a static array of users.
    Provides a fixed list of sample users.

--- src/main/java/student/model/User/UserArrayRepository.java
+++ src/main/java/student/model/User/UserArrayRepository.java
@@ -1,4 +1,30 @@
 package student.model.User;
 
-public class UserArrayRepository {
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * A repository that provides users from a static array.
+ */
+public class UserArrayRepository implements UserRepository {
+
+    private static final User[] users;
+
+    static {
+        users = new User[]{
+                new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "James"),
+                new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Jamila")
+        };
+    }
+
+    /**
+     * Returns a list of users from a static array.
+     *
+     * @return a list of users
+     */
+    @Override
+    public List<User> getUsers() {
+        return Arrays.asList(users);
+    }
 }
